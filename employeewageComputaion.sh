a2b3173efb9941858f33d5aec0bc11fe5ddb4678
mayavathi <mayachandre27@gmail.com>
2021-06-04 04:12:25 -0400

    employee is present or absent

--- employeewageComputaion.sh
+++ employeewageComputaion.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo "Welcome to Employee Wage Computation Program"
+eclatre -a arr
+wagePerHr=20
+fullHr=20
+partHr=4
+wage=0
+dailywage=0
+
+isAbsent=0
+isPart=1
+isFull=2
+day=0
+totalHrs=0
